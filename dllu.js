4f3e51622aca866bef2db577b33c3dff7ff6f145
Daniel Lu <daniel@athinkingape.com>
2012-08-12 00:59:48 -0700

    ajax loading of snippets now works

--- dllu.js
+++ dllu.js
@@ -1,23 +1,80 @@
 $(document).ready( function() {
     $('#javascript-alert').remove();
 
-    var evaluatePath = function() {
-        var working_directory = '/' + window.location.hash.split('#')[1];
-        console.log(working_directory);
-    }
+    var renderContent = function(data, contentInside) {
+        contentInside.html(data);
+        contentInside.find('.nav a, .portfolio a').click(bindNavigationLinks);
+        contentInside.css({height: contentInside.children().innerHeight() + 'px'});
+        contentInside.find('a').each(function() {
+            if(window.location.hash.indexOf($(this).attr('href')) !== -1) $(this).addClass('selected');
+        });
+    };
 
-    $('.nav a, .portfolio a').click(function(e) {
+    var loadContent = function(path, contentInside) {
+        var is_undefined = contentInside === undefined;
+        if(is_undefined) {
+            contentInside = $('<div>').addClass('content-inside');
+            $('#content').append(contentInside);
+        }
+        $.get(path, function(data) {
+            renderContent(data, contentInside);
+        }, 'html').error(function(data, textStatus, jqXHR) {
+            console.log(jqXHR);
+            renderContent('<section><h1>Error: ' + jqXHR.code + '</h1></section>', contentInside);
+        });
+    };
+
+    var bindNavigationLinks = function(e) {
         e.preventDefault;
-        var old_working_directory = '/' + window.location.hash.split('#')[1];
         window.location.hash = $(this).attr('href');
         evaluatePath();
-    });
-    var loadContent = function(e) {
-        e.preventDefault();
-        $(this).css({'background-color':'#dde'})
-        var newContent = $('<div>').addClass('content-inside');
-        newContent.click(addContent);
-        $(this).parent().append(newContent);
     };
-    //$('.content-inside').click(addContent);
+
+    var evaluatePath = function() {
+        var working_directory = '';
+        if(window.location.hash.indexOf('#') !== -1) {
+            working_directory = window.location.hash.split('#')[1];
+        }
+        var working_directory_split = working_directory.split('/');
+
+        var i = working_directory_split.length;
+        console.log(working_directory_split, i);
+        if(working_directory_split[i-1] === undefined) i--; //remove effect of trailing slash
+        if(working_directory === '') i = 0;
+
+        var firstContentInside = $('#content').children('.content-inside')[0];
+        if(firstContentInside !== undefined) {
+            firstContentInside = $(firstContentInside);
+        }
+        if(i === 0) {
+            loadContent('main.html', firstContentInside);
+        } else {
+            loadContent('nav.html', firstContentInside);
+        }
+
+        var path = '';
+        var contentChildren = $('#content').children('.content-inside');
+        var iterationLength = contentChildren.length;
+        for(var j=1; j<=i; j++) {
+            path += working_directory_split[j-1] + '/';
+            var contentInside = contentChildren[j] === undefined? undefined : $(contentChildren[j])
+            if(j<i) {
+                console.log('/branch', path);
+                loadContent(path + 'nav.html', contentInside);
+            } else {
+                console.log('/node', path);
+                loadContent(path + 'main.html', contentInside);
+            }
+        }
+        console.log(i, iterationLength);
+        for(var j=i+1; j<iterationLength; j++){
+            $(contentChildren[j]).remove();
+        }
+    };
+    $('#dllu').click( function(e) {
+        e.preventDefault();
+        window.location.hash = '';
+        evaluatePath();
+    });
+    evaluatePath();
 });
